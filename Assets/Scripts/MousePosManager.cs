0ef2750fee82ae3cedce53b44cf34d33f55dda42
Bram <bram.salomons@gmail.com>
2026-06-01 11:30:41 +0200

    Made camera flexible

--- Assets/Scripts/MousePosManager.cs
+++ Assets/Scripts/MousePosManager.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class MousePosManager : MonoBehaviour
 {
+
+    [SerializeField] private bool resetWorldOnScreen;
+
+    [SerializeField] private Transform placeableMin;
+    [SerializeField] private Transform placeableMax;
+
+    [Header("Set automatically")]
     [SerializeField] private Vector2 blockPlaceMin;
     [SerializeField] private Vector2 blockPlaceMax;
 
@@ -11,22 +18,32 @@ public class MousePosManager : MonoBehaviour
 
     [SerializeField] private Transform currentSelectionViewer;
 
+
+    [SerializeField] private Vector2 worldOnScreenMin;
+    [SerializeField] private Vector2 worldOnScreenMax;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ResetWorldOnScreen();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(Input.mousePosition.x/Screen.width + " " + Input.mousePosition.y/Screen.height);
+        if (resetWorldOnScreen)
+        {
+            ResetWorldOnScreen();
+        }
+        Vector3 MTW = GetMouseToWorld();
+
+        // Inputs
         if (Input.GetMouseButton(0))
         {
             RaycastHit hitInfo;
-            if (Physics.Raycast(GetMouseToWorld() - Vector3.forward, Vector3.forward, out hitInfo))
+            if (Physics.Raycast(MTW - Vector3.forward, Vector3.forward, out hitInfo))
             {
-                if (hitInfo.transform != null)
+                if (hitInfo.transform != null && hitInfo.transform.GetComponent<PlaceableBlock>() != null)
                 {
                     selectedObj = hitInfo.transform;
                 }
@@ -37,15 +54,18 @@ public class MousePosManager : MonoBehaviour
             selectedObj = null;
             
         }
-        pos.x = Mathf.Round(Input.mousePosition.x / Screen.width * 18);
-        pos.y = Mathf.Round(Input.mousePosition.y / Screen.height * 10);
+
+
+        // Setting position
+        pos.x = Mathf.Round(MTW.x);
+        pos.y = Mathf.Round(MTW.y);
+
 
         if (pos.x < blockPlaceMin.x || pos.x > blockPlaceMax.x ||
             pos.y < blockPlaceMin.y || pos.y > blockPlaceMax.y)
         {
             pos = currentSelectionViewer.position;
         }
-
 
 
         if (selectedObj != null) 
@@ -55,8 +75,28 @@ public class MousePosManager : MonoBehaviour
 
     }
 
+    // Recalibrate the camera to make the mouse position match world position
+    private void ResetWorldOnScreen()
+    {
+        Vector3 camPos = Camera.main.transform.position;
+        float heightOrthoSize = Camera.main.orthographicSize;
+        float widthOrthoSize = heightOrthoSize * Screen.width / Screen.height;
+        worldOnScreenMin = new Vector2(camPos.x - widthOrthoSize, camPos.y - heightOrthoSize);
+        worldOnScreenMax = new Vector2(camPos.x + widthOrthoSize, camPos.y + heightOrthoSize);
+
+        if (placeableMin == null || placeableMax == null)
+        {
+            Debug.LogError("Min and/or Max placement position isn't defined");
+            return;
+        }
+        blockPlaceMin = new Vector2(placeableMin.position.x, placeableMin.position.y);
+        blockPlaceMax = new Vector2(placeableMax.position.x, placeableMax.position.y);
+    }
+
     private Vector3 GetMouseToWorld()
     {
-        return new Vector3(Input.mousePosition.x / Screen.width * 18, Input.mousePosition.y / Screen.height * 10, 0);
+        float xPos = Mathf.Lerp(worldOnScreenMin.x, worldOnScreenMax.x, Input.mousePosition.x / Screen.width);
+        float yPos = Mathf.Lerp(worldOnScreenMin.y, worldOnScreenMax.y, Input.mousePosition.y / Screen.height);
+        return new Vector3(xPos, yPos, 0);
     }
 }
